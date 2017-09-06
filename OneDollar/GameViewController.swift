4ded14e84ff332d4f2e8f156c212b7fcd392ed3a
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2017-09-06 14:37:43 -0300

    One Dollar Recognizer algorithm based off of Daniele Margutti's Swift 2 implementation of Wobbrock, Wilson and Li 2007 paper.

---
+++ OneDollar/GameViewController.swift
@@ -0,0 +1,140 @@
+//
+//  GameViewController.swift
+//  OneDollar
+//
+//  Created by Bruno Omella Mainieri on 06/09/17.
+//  Based on example by Daniele Margutti (02/10/15)
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    private var loadedTemplates:[SwiftUnistrokeTemplate] = []
+    private var templateViews:[StrokeView] = []
+//    var drawView:StrokeView!
+//    var templatesScrollView	:UIScrollView!
+//    var labelTemplates:UILabel!
+    
+    @IBOutlet weak var drawView: StrokeView!
+    @IBOutlet weak var templatesScollView: UIScrollView!
+    @IBOutlet weak var labelTemplates: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        if let view = self.view as! SKView? {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//                
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+//            
+//            view.ignoresSiblingOrder = true
+//            
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
+        
+        
+        loadTemplatesDirectory()
+        
+        drawView.backgroundColor = UIColor.cyan
+        drawView.onDidFinishDrawing = { drawnPoints in
+            if drawnPoints == nil {
+                return
+            }
+            
+            if drawnPoints!.count < 5 {
+                return
+            }
+            
+            let strokeRecognizer = OneDollar(points: drawnPoints!)
+            do {
+                let (template,distance) = try strokeRecognizer.recognizeIn(templates: self.loadedTemplates, useProtractor: false, minThreshold: 0.80)
+                
+                self.labelTemplates.text = "Recognized: \(template!.name.uppercased())"
+                
+                
+            } catch (let error as NSError) {
+                print("FAILED WITH ERROR: \(error.localizedDescription)")
+                self.labelTemplates.text = "No gesture recognized"
+            }
+            
+        }
+        
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    private func loadTemplatesDirectory() {
+        do {
+            // Load template files
+            let templatesFolder = Bundle.main.resourcePath!.appending("/Templates")
+            
+            let list = try FileManager.default.contentsOfDirectory(atPath: templatesFolder)
+            
+            var x:CGFloat = 0.0
+            let size = templatesScollView.frame.height
+            for file in list {
+                let templateData = NSData(contentsOfFile: templatesFolder.appendingFormat("/%@", file))
+                let templateDict = try JSONSerialization.jsonObject(with: templateData! as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
+                let templateName = templateDict["name"]! as! String
+                let templateImage = templateDict["image"]! as! String
+                let templateRawPoints: [AnyObject] = templateDict["points"]! as! [AnyObject]
+                var templatePoints: [StrokePoint] = []
+                for rawPoint in templateRawPoints {
+                    let x = (rawPoint as! [AnyObject]).first! as! Double
+                    let y = (rawPoint as! [AnyObject]).last! as! Double
+                    templatePoints.append(StrokePoint(x: x, y: y))
+                }
+                
+                let templateObj = SwiftUnistrokeTemplate(name: templateName, points: templatePoints)
+                loadedTemplates.append(templateObj)
+                print("  - Loaded template '\(templateName)' with \(templateObj.points.count) points inside")
+                
+                // For each template get its preview and show them inside the bottom screen scroll view
+                let templateView = UIImageView(frame: CGRect(x:x,y:0,width:size,height:size))
+                templateView.image = UIImage(named: templateImage)
+                templateView.contentMode = UIViewContentMode.scaleAspectFit
+                templateView.layer.borderColor = UIColor.lightGray.cgColor
+                templateView.layer.borderWidth = 2
+                templatesScollView.addSubview(templateView)
+                x = templateView.frame.maxX+2
+            }
+            
+            print("- \(loadedTemplates.count) templates are now loaded!")
+            
+            // setup scroll view size
+            templatesScollView.contentSize = CGSize(width:x+CGFloat(2*loadedTemplates.count), height:size)
+            templatesScollView.backgroundColor = UIColor.white
+            labelTemplates.text = "\(loadedTemplates.count) TEMPLATES LOADED:"
+        } catch (let error as NSError) {
+            print("Something went wrong while loading templates: \(error.localizedDescription)")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ OneDollar/StrokeView.swift
@@ -0,0 +1,83 @@
+//
+//  StrokeView.swift
+//  OneDollar
+//
+//  Created by Bruno Omella Mainieri on 06/09/17.
+//  Based on example by Daniele Margutti (02/10/15)
+//
+
+import Foundation
+import UIKit
+
+typealias StrokeViewEndBlock = (_ points: [StrokePoint]?) -> (Void)
+
+public class StrokeView : UIView {
+    
+    var drawPath: UIBezierPath
+    var onDidFinishDrawing: StrokeViewEndBlock?
+    var activePoints = [StrokePoint]()
+    
+    override init(frame: CGRect) {
+        drawPath = UIBezierPath()
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.lightGray
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        drawPath = UIBezierPath()
+        super.init(coder: aDecoder)
+    }
+    
+    public func loadPath(points: [StrokePoint]) {
+        self.drawPath = UIBezierPath()
+        if points.count > 0 {
+            self.drawPath.move(to: points.first!.toPoint())
+            for i in 1..<points.count {
+                self.drawPath.addLine(to: points[i].toPoint())
+            }
+            self.setNeedsDisplay()
+        }
+    }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.drawPath = UIBezierPath()
+        self.drawPath.lineWidth = 3.0
+        
+        activePoints.removeAll()
+        
+        let point = touches.first!.location(in: self)
+        self.drawPath.move(to: point)
+        activePoints.append(StrokePoint(point: point))
+        
+        self.setNeedsDisplay()
+    }
+    
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let point = touches.first!.location(in: self)
+        self.drawPath.addLine(to: point)
+        activePoints.append(StrokePoint(point: point))
+        
+        self.setNeedsDisplay()
+    }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let point = touches.first!.location(in: self)
+        self.drawPath.move(to: point)
+        activePoints.append(StrokePoint(point: point))
+        self.setNeedsDisplay()
+        onDidFinishDrawing?(activePoints)
+    }
+    
+    public override func touchesCancelled(_ touches: Set<UITouch>?, with event: UIEvent?) {
+        if touches != nil {
+            self.touchesEnded(touches!, with: event)
+        }
+    }
+    
+    public override func draw(_ rect: CGRect) {
+        let ctx = UIGraphicsGetCurrentContext()
+        ctx!.setLineWidth(3.0)
+        ctx!.addPath(self.drawPath.cgPath)
+        ctx!.strokePath()
+    }
+}
